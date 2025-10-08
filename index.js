4cc2ddab4324bf5aa0ee0ae7ed41ae52cf0c0620
Dam <dfnex10@gmail.com>
2025-10-08 09:32:16 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,97 @@
+  {
+    name: "Chips",
+    price: 4.50,
+    inStock: true,
+    id: "food",
+    img: "https://cdn11.bigcommerce.com/s-840m18ew7o/products/3248/images/4866/44359__19670.1592331568.386.513.jpg?c=1"
+  },
+  {
+    name: "Soda",
+    price: 2.50,
+    inStock: true,
+    id: "drink",
+    img: "https://th.bing.com/th/id/R.a7caeda5965272bbce7af548727e2155?rik=Re6%2f66XLASoodA&riu=http%3a%2f%2fsc02.alicdn.com%2fkf%2fUTB8hq1MxVfFXKJk43Otq6xIPFXaL%2fCoca-Cola-330ml.jpg&ehk=G%2fOjbOiVtxBzFeGgjDtc%2bomj5VNqtVRZEr9nTMlS5XQ%3d&risl=&pid=ImgRaw&r=0"
+  },
+  {
+    name: "Juice",
+    price: 1.00,
+    inStock: true,
+    id: "drink",
+    img: "https://images.heb.com/is/image/HEBGrocery/001071026-1"
+  },
+  {
+    name: "Gas can",
+    price: 40.00,
+    inStock: true,
+    id: "other",
+    img: "https://mobileimages.lowes.com/product/converted/044549/044549800332.jpg"
+  },
+  {
+    name: "Peanuts",
+    price: 1.92,
+    inStock: true,
+    id: "food",
+    img: "https://m.media-amazon.com/images/I/51q+SKvitlL.jpg"
+  },
+  {
+    name: "Alcohol",
+    price: 2.69,
+    inStock: true,
+    id: "drink",
+    img: "https://i5.walmartimages.com/asr/8f0a9d7c-bb1c-4bef-aace-cf12f520c19a_1.de218dd3241f1c4c087bf9e4a740cc7b.jpeg"
+  },
+  {
+    name: "Cigarettes",
+    price: 16.00,
+    inStock: true,
+    id: "other",
+    img: "https://images.gopuff.com/blob/gopuffcatalogstorageprod/catalog-images-container/resize/cf/version=1_2,format=auto,fit=scale-down,width=800,height=800/b086c5e6-2c22-4bde-af48-b2e615afcf16-background_removed.png"
+  },
+  {
+    name: "Coffee",
+    price: 5.00,
+    inStock: true,
+    id: "drink",
+    img: "https://4.imimg.com/data4/NM/VH/MY-978282/coffee-paper-cups-500x500.jpg"
+  },
+  {
+    name: "Breakfast Sandwich",
+    price: 6.00,
+    inStock: true,
+    id: "food",
+    img: "https://media.istockphoto.com/photos/bagel-with-scrambled-eggs-and-bacon-isolated-on-white-picture-id475835518?k=20&m=475835518&s=612x612&w=0&h=hszmOvwECdHPASCIZWZ1VbXfC9dTnsZYqxSsdeOg2mw="
+  },
+  {
+    name: "BLT",
+    price: 8.00,
+    inStock: true,
+    id: "food",
+    img: "https://thumbs.dreamstime.com/b/tasty-bacon-lettuce-tomato-blt-sandwich-white-background-324702360.jpg"
+  },
+  {
+    name: "Pretzel",
+    price: 2.50,
+    inStock: true,
+    id: "food",
+    img: "https://img.freepik.com/premium-photo/illustration-white-background-delicious-pretzels-image_435599-29489.jpg"
+  },
+  {
+    name: "Chicken",
+    price: 10.00,
+    inStock: true,
+    id: "food",
+    img: "https://tse2.mm.bing.net/th/id/OIP.sdYMm8mPyU9zQon3vlPUlQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
+  },
+  {
+    name: "Water",
+    price: 2.50,
+    inStock: true,
+    id: "drink",
+    img: "https://media1.popsugar-assets.com/files/thumbor/oUUQDbG6yDYSgG-5BQS6QRGj0RE/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2015/10/28/798/n/1922398/fb0bffcf28178694_spin_prod_ec_1567913002.jpeg"
+  }
+];
+
+products.forEach((products) => makeCard(products));
+function makeCard(item) {
+    
+}
